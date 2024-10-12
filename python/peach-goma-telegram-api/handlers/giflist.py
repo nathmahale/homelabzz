591bc38bc75317f2b0b4dc06604f346d27a6ac76
nathmahale <16042612+nathmahale@users.noreply.github.com>
2024-10-12 23:28:01 +1100

    fixed gif file loading

--- python/peach-goma-telegram-api/handlers/giflist.py
+++ python/peach-goma-telegram-api/handlers/giflist.py
@@ -1,9 +1,8 @@
 from aiogram import Router
 from aiogram.filters import Command
-from aiogram.types import Message
-from aiogram.types.input_file import FSInputFile
+from aiogram.types import Message, FSInputFile
 from os import listdir
-from os.path import isfile, join, pardir
+from os.path import isfile, join
 import os
 import logging
 
@@ -14,21 +13,22 @@ giflist_router = Router()
 async def command_giflist_handler(message: Message) -> None:
     """
     This handler receives messages with `/giflist` command
-    python/peach-goma-telegram-api/gifs
     """
     gifStorageDirectory = join(os.getcwd(), 'gifs')
     logging.info(f"{gifStorageDirectory=}")
 
-    giffileList = [f for f in listdir(gifStorageDirectory) if isfile(join(gifStorageDirectory, f))]
+    giffileList = [f for f in listdir(
+        gifStorageDirectory) if isfile(join(gifStorageDirectory, f))]
 
     for giffilename in giffileList:
-        gif_caption = "gif_caption"
+        gif_caption = giffilename.split(".")[0]
         logging.info(f"{giffilename=}")
-        await message.reply_animation(
-            # animation= open(giffilename, "rb").read(),
-            animation= FSInputFile(path= gifStorageDirectory,
-                                   filename= giffilename),
+        await message.animation(
+            animation=FSInputFile(path='/'.join(
+                (gifStorageDirectory, giffilename))
+            ),
             caption=gif_caption,
             width=20,
             height=20
         )
+        logging.info(f"{message.get_url=}")
